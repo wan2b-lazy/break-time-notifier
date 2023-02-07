ddede68932e61a03016cb2bcd0eae0a53a2ef660
satsuka <wan2b.lazy@gmail.com>
2023-02-07 15:41:51 +0900

    indexページに必要な要素をすべて追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>アラーム設定</h1>
+
+      <form action="#">
+        <ul>
+          <li>
+            <label htmlFor="#">間隔（分）</label>
+            <input type="number" name="#" id="#" />
+          </li>
+          <li>
+            <label htmlFor="#">終了予定</label>
+            <input type="number" name="#" id="#" />
+          </li>
+          <li>
+            <button type="button">活動開始</button>
+          </li>
+        </ul>
+      </form>
     </>
   )
 }
