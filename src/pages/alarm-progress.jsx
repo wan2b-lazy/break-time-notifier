78768906f2d92a7515e7c854786fc008b3fa7d84
satsuka <wan2b.lazy@gmail.com>
2023-03-01 11:59:12 +0900

    alarm-progressページのひな型を作成

---
+++ src/pages/alarm-progress.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/globals.module.scss";
+
+export default function AlarmProgress() {
+  return (
+    <>
+      <h1 className={styles.title}>alarm-progress-page</h1>
+    </>
+  );
+}
