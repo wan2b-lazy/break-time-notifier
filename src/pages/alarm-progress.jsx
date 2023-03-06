bffc46d1465585aef1b28acf04c7921415d04b33
satsuka <wan2b.lazy@gmail.com>
2023-03-06 12:25:33 +0900

    alarm-progress.jsxの基本的なスタイル付けが完了

--- src/pages/alarm-progress.jsx
+++ src/pages/alarm-progress.jsx
@@ -3,7 +3,9 @@ import styles from "../styles/globals.module.scss";
 export default function AlarmProgress() {
   return (
     <>
-      <h1 className={styles.title}>alarm-progress-page</h1>
+      <h1 className={styles.title}>次回のアラーム予定は</h1>
+      <h2 className={styles.title}>12:00</h2>
+      <button className={styles.button}>アラームを中断</button>
     </>
   );
 }
