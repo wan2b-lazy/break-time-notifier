d1122a5a7410510d0a03ac0f66753c4f12d93896
satsuka <wan2b.lazy@gmail.com>
2023-03-06 12:59:40 +0900

    alarm-notificationファイルのひな型とテストファイルを作成

---
+++ src/pages/alarm-notification.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/globals.module.scss";
+
+export default function AlarmNotification() {
+  return (
+    <>
+      <h1 className={styles.title}>休憩時間です</h1>
+      <button className={styles.button}>ストレッチ開始</button>
+      <button className={styles.button}>ストレッチ完了</button>
+      <button className={styles.button}>中断</button>
+    </>
+  );
+}
