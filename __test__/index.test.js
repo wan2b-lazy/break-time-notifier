1ab99609f56c1d7f79563ec29e482ef3c879cb0b
satsuka <wan2b.lazy@gmail.com>
2023-03-11 22:07:54 +0900

    index.jsx用のテストファイルにuseRouterをモックアウトするための記述を追加

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,6 +2,15 @@ import { render, screen } from "@testing-library/react";
 import Home from "../src/pages/index";
 import "@testing-library/jest-dom";
 
+//next/routerをmockにすることで、テスト時にエラーが起こらないようにする
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+    };
+  },
+}));
+
 describe("Home", () => {
   test("render a heading", () => {
     render(<Home />);
